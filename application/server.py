cfab7094a93b7e455f134b420860af182c3035a2
ClGide <giderutazihana81@gmail.com>
2022-07-10 20:42:54 +0200

    just to see

---
+++ application/server.py
@@ -0,0 +1,139 @@
+"""
+Whenever we refer to clubs or competitions, we refer to dictionary
+holding (possibly modified by user interaction) data about the clubs
+or competitions from the JSON files. The keys of those dictionary are
+always strings while the values can be strings, integers, booleans and
+even dictionaries.
+
+Don't change the import names or file paths, they are relative to Project11
+directory; the place from which we run the app.
+"""
+
+from flask import Blueprint, render_template, \
+    request, redirect, flash, url_for
+from application import load_clubs, search_club, \
+    load_competitions, search_competition, \
+    run_checks, update_all_competitions_taken_place_field, \
+    record_changes
+
+
+CLUB_PATH = "clubs.json"
+COMPETITION_PATH = "competitions.json"
+
+bp = Blueprint("gudlft", __name__, url_prefix="")
+
+
+@bp.route('/')
+def index():
+    clubs = load_clubs(CLUB_PATH)
+    return render_template('index.html',
+                           clubs=clubs)
+
+
+@bp.route("/backToSummary/<email>")
+def come_back_welcome_page(email):
+    """
+    Loads the available competitions in welcome.html and the data about the
+    club that was logged_in in booking.html.
+    """
+    competitions = load_competitions(COMPETITION_PATH)
+    club, clubs = search_club("email", email, CLUB_PATH)
+    return render_template('welcome.html',
+                           club=club,
+                           competitions=competitions)
+
+
+@bp.route('/showSummary', methods=['POST'])
+def show_summary():
+    """
+    Loads the available competitions in welcome.html and the data about the
+    club that just logged in in index.html. It handles the form in index.html.
+    """
+    club, clubs = search_club("email", request.form['email'], CLUB_PATH)
+    competitions = load_competitions(COMPETITION_PATH)
+    if club:
+        competitions = update_all_competitions_taken_place_field(
+            competitions, COMPETITION_PATH
+        )
+        return render_template('welcome.html',
+                               club=club,
+                               clubs=clubs,
+                               competitions=competitions)
+
+    flash("we couldn't find your email in our database.")
+    return render_template("index.html")
+
+
+@bp.route(
+    '/book/<competition_to_be_booked_name>/<club_making_reservation_name>'
+)
+def book(competition_to_be_booked_name, club_making_reservation_name):
+    """
+    Shows the user which competition he can book places to.
+
+    In the previous implementation there was an if statement that seemed
+    superfluous to me. The logic was the following. If for some reason, the
+    webpage shows some competition which disappeared, and the user tries to
+    book a place at that competition, the flash message will them that something
+    went wrong. But I don't see any reason for that to happen. The user never
+    manually enters a competition's name.
+    Ideally, I shouldn't even have to reload the JSON file. But for the moment
+    it seems I can only send a str repr of a dict and not a dict through url_for.
+    In other words, the value passed to competition_to_be_booked isn't a dict
+    but a str repr of a dict or a dict.
+    """
+    competition, competitions = search_competition(
+        "name",
+        competition_to_be_booked_name,
+        COMPETITION_PATH
+    )
+    club, clubs = search_club("name", club_making_reservation_name, CLUB_PATH)
+    return render_template('booking.html',
+                           club=club,
+                           competition=competition)
+
+
+@bp.route('/purchasePlaces', methods=['POST'])
+def purchase_places():
+    """Handles the form in booking.html."""
+
+    competition_to_be_booked_name = request.form['competition']
+    competition, competitions = search_competition(
+        "name",
+        competition_to_be_booked_name,
+        COMPETITION_PATH
+    )
+    club, clubs = search_club("name", request.form["club"], CLUB_PATH)
+    places_required = int(request.form['places'])
+    club_number_of_points = int(club["points"])
+
+    failed_checks = run_checks(competition, club, places_required,
+                               club_number_of_points)
+    if failed_checks:
+        return failed_checks
+
+    competitions, club = record_changes(competitions, competition, clubs, club,
+                                        places_required, club_number_of_points,
+                                        COMPETITION_PATH, CLUB_PATH)
+    flash('Great-booking complete!')
+    return render_template('welcome.html',
+                           club=club,
+                           clubs=clubs,
+                           competitions=competitions)
+
+
+@bp.route("/points")
+def points():
+    clubs_to_display = load_clubs(CLUB_PATH)
+    return render_template("points.html",
+                           clubs=clubs_to_display)
+
+
+@bp.route('/logout')
+def logout():
+    return redirect(url_for('gudlft.index'))
+
+
+def just_to_see():
+    return
+
---
+++ application/utils.py
@@ -0,0 +1,266 @@
+"""Whenever we refer to clubs or competitions, we refer to dictionary
+holding (possibly modified by user interaction) data about the clubs
+or competitions from the JSON files. The keys of those dictionary are
+always strings while the values can be strings, integers, booleans and
+even dictionaries.
+"""
+
+import json
+from datetime import datetime
+from typing import Union
+
+from flask import flash, render_template
+
+
+def load_clubs(path) -> list[dict[str, any]]:
+    """Loads the JSON file containing data about the clubs and
+    returns it in a list."""
+    with open(path) as clubs:
+        list_of_clubs = json.load(clubs)['clubs']
+        return list_of_clubs
+
+
+def load_competitions(path) -> list[dict[str, any]]:
+    """Loads the JSON file containing data about the competitions and
+    returns it in a list."""
+    with open(path) as competitions:
+        list_of_competitions = json.load(competitions)['competitions']
+        return list_of_competitions
+
+
+def search_club(field: str, value: any, path: str) -> Union[list, tuple[
+    dict[str, any], list[dict[str, any]]]]:
+    """Loads the JSON file containing data about the clubs and
+    returns data about the club having the corresponding field and
+    value. For convenience, also returns the list of clubs."""
+    if field not in ["name", "email", "points", "reserved_places"]:
+        raise ValueError("the value for the field arg isn't a valid one")
+
+    clubs = load_clubs(path)
+    try:
+        club = [club for club in clubs if club[field] == value][0]
+    except IndexError or KeyError:
+        return []
+    else:
+        return club, clubs
+
+
+def search_competition(field: str, value: any, path: str) -> Union[list, tuple[
+    dict[str, any], list[dict[str, any]]]]:
+    """Loads the JSON file containing data about the competitions and only
+    returns data about the competition having the corresponding field and
+    value. For convenience, also returns the list of clubs."""
+    if field not in ["name", "date", "number_of_places", "taken_place"]:
+        raise ValueError("the value for the field arg isn't a valid one")
+
+    competitions = load_competitions(path)
+    try:
+        competition = [competition for competition in competitions
+                       if competition[field] == value][0]
+    except IndexError or KeyError:
+        return []
+    else:
+        return competition, competitions
+
+
+def update_all_competitions_taken_place_field(
+        competitions: list[dict[str, any]],
+        competition_path: str) -> list[dict[str, any]]:
+    """Receives a list of competitions. Loops through each
+    competition. Compares the date field with datetime.now(). Sets
+    the taken_place field to True if the date field is behind.
+    Writes the list of competitions to the JSON file and returns
+    the list."""
+    for competition in competitions:
+        if datetime.strptime(
+                competition["date"], "%Y-%m-%d %H:%M:%S"
+        ) <= datetime.now():
+            competition["taken_place"] = True
+        else:
+            competition["taken_place"] = False
+            with open(
+                    competition_path, "w"
+            ) as file_to_write_competitions:
+                json.dump({"competitions": competitions},
+                          file_to_write_competitions,
+                          indent=4)
+    return competitions
+
+
+def more_than_12_reserved_places(club_reserved_places: int,
+                                 required_places: int) -> Union[str, None]:
+    """Flashes a message if the club did reserve more than 12 places.
+
+    Args:
+        club_reserved_places: the number of places the club already reserved at
+            the tournament before this operation.
+        required_places: the number of places the club wants to reserve at the
+            tournament within this operation.
+
+    Returns: A string used in run_checks() if the club tries to reserve more
+        than 12 places.
+    """
+    to_be_reserved_total_places = club_reserved_places + required_places
+    if to_be_reserved_total_places > 12:
+        flash("you required more than 12 places !")
+        return "failed_check"
+
+
+def not_enough_points(required_places: int, club_number_of_points: int) -> \
+        Union[str, None]:
+    """Flashes the corresponding message if the club wants to purchase more
+    places than they have points.
+
+    Args:
+        required_places: the number of places the club wants to reserve at the
+            tournament within this operation
+        club_number_of_points: the number of points the club has before this
+            operation.
+
+    Returns: A string used in run_checks() if the club wants to purchase more
+    places than they have points.
+    """
+    if required_places > club_number_of_points:
+        flash("you do not have enough points!")
+        return "failed_check"
+
+
+def no_more_available_places(required_places: int, places_available: int) -> \
+        Union[str, None]:
+    """Flashes the corresponding message if there aren't enough places at the
+    competition.
+
+    Args:
+        required_places: the number of places the club wants to reserve at the
+            tournament within this operation.
+        places_available: the competition's number of available places.
+
+    Returns: A string used in run_checks() if the club wants to purchase
+        places although the competition doesn't have places anymore.
+    """
+    if places_available - required_places < 0:
+        flash("there are no more places available !")
+        return "failed_check"
+
+
+def competition_took_place(competition: dict[str, any]) -> Union[str, None]:
+    """Flashes the corresponding message if the competition already took place.
+
+    There is already a check when welcome.html is loaded. However, it might be
+    the case that after first loading the page, the user is inactive some time
+    and then tries to purchase a place. Therefore, a check at that stage is
+    also useful.
+
+    Args:
+        competition: the competition where the club wants to reserve a place
+            within this operation.
+
+    Returns: A string used in run_checks() if the club wants to purchase
+        places although the competition already took place.
+    """
+    if datetime.strptime(
+            competition["date"], "%Y-%m-%d %H:%M:%S"
+    ) <= datetime.now():
+        competition["taken_place"] = True
+    else:
+        competition["taken_place"] = False
+    if competition["taken_place"]:
+        flash("the competition already took place !")
+        return "failed_check"
+
+
+def run_checks(competition: dict[str, any], club: dict[str, any],
+               required_places: int, club_number_of_points: int) -> callable:
+    """Makes sure all conditions are met to enable the club to purchase the
+    required places at the competition.
+
+    The first unmet condition will trigger a flash message via one of the
+    4 functions call. Each function calls checks one condition. If one checks
+    failed, the following checks aren't done.
+
+    Args:
+        competition: the competition where the club wants to purchase places
+            within this operation.
+        club: the club trying to purchase places.
+        required_places: the number of places the club wants to reserve at the
+            tournament within this operation.
+        club_number_of_points: the number of points the club has before this
+            operation.
+
+    Returns: A call to render_template. It renders booking.html from the
+        templates directory with the club and competition vars as a context.
+    """
+
+    failed_checks = more_than_12_reserved_places(
+        club["reserved_places"][competition["name"]],
+        required_places
+    ) or not_enough_points(
+        required_places,
+        club_number_of_points
+    ) or no_more_available_places(
+        required_places,
+        competition["number_of_places"]
+    ) or competition_took_place(competition)
+
+    if failed_checks:
+        return render_template("booking.html",
+                               club=club,
+                               competition=competition)
+
+
+def record_changes(competitions: list[dict[str, any]],
+                   competition: dict[str, any],
+                   clubs: list[dict[str, any]],
+                   club: dict[str, any],
+                   required_places: int,
+                   club_number_of_points: int,
+                   competition_path: str,
+                   club_path: str
+                   ) -> tuple[
+                    list[dict[str, any]], dict[str, any]]:
+    """Writes two new JSON files to record the changes after the club
+     successfully purchased places to the competition.
+
+     Helper function used in server.purchase_places.
+
+    Args:
+        competitions: all the competitions. Even if only one competition
+            was modified, we need to write them all back to the JSON.
+        competition: the competition where the club wants to purchase places
+            within this operation.
+        clubs: all the competitions. Even if only one competition
+            was modified, we need to write them all back to the JSON.
+        club: the club trying to purchase places.
+        required_places: the number of places the club wants to reserve at the
+            tournament within this operation.
+        club_number_of_points: the number of points the club has before this
+            operation.
+        competition_path: path to the JSON file storing the competitions.
+        club_path: path to the JSON file storing the clubs.
+
+    Returns: All the competitions and the club that successfully purchased
+        the places. Those dictionaries are used by the render_template call
+        returned by server.purchase_places.
+    """
+    competition_to_be_booked_name = competition["name"]
+    competition['number_of_places'] = int(
+        competition['number_of_places']
+    ) - required_places
+
+    club["points"] = club_number_of_points - required_places
+
+    reserved_places = club["reserved_places"][competition_to_be_booked_name]
+    total_reserved_places = reserved_places + required_places
+    club["reserved_places"][
+        competition_to_be_booked_name] = total_reserved_places
+    with open(club_path, "w") as to_be_updated_clubs:
+        json.dump({"clubs": clubs},
+                  to_be_updated_clubs,
+                  indent=4)
+
+    with open(competition_path, "w") as to_be_updated_competitions:
+        json.dump({"competitions": competitions},
+                  to_be_updated_competitions,
+                  indent=4)
+
+    return competitions, club
